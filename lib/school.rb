27b680ebe7649d1994c84c69119e76a783832aa9
Joshua M Loran <joshua.loran@gmail.com>
2019-02-20 23:56:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
-# code here!
+class School
+  attr_accessor :school_name, :roster, :student, :age
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def add_student(student, age)
+    @roster 
+  end
+end

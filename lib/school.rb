2f3ec47673aa88bc1e5c40097bec9f71cb2d04f5
Joshua M Loran <joshua.loran@gmail.com>
2019-02-21 00:22:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,7 @@ class School
   end
   
   def add_student(student, grade)
-    if @roster[grade].values.length == 1
+    if @roster.values.length == 0
       @roster[grade] = [] << student
     else
       @roster[grade] << student
